32615401f55ecb199342b39f0d8b74ff60cc31ec
Ben Cuello-Wolffe <bcuellowolffe@gmail.com>
2024-06-29 11:37:12 +0200

    Initial Commit

---
+++ CloudBusting/CloudBustingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudBustingApp.swift
+//  CloudBusting
+//
+//  Created by Ben Cuello-Wolffe on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CloudBustingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
